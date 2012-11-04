f1692c782b72606031455908e54a556d4c9b6678
Julien Rouviere <julien.rouviere@gmail.com>
2012-11-04 11:36:53 +0100

    Initial uart driver. Old sources Cleanup. FIXME: Uart reception bug on the smp.

---
+++ Embedded/src/pc_comm_uart.h
@@ -0,0 +1,27 @@
+/*
+ *  This file is part of OpenSCB project <http://openscb.org>.
+ *  Copyright (C) 2010  Opendrain
+ *
+ *  OpenSCB software is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  OpenSCB software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with OpenSCB software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef PC_COMM_UART_H_
+#define PC_COMM_UART_H_
+
+
+void uart_init(void);
+
+
+#endif /* PC_COMM_UART_H_ */
